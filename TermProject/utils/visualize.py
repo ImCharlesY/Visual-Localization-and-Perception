4615b2cc1c5a134671959a7601abbec4793fd579
ImCharlesY <ImCharles1025@gmail.com>
2018-12-30 12:44:08 +0800

    TermPrject first 7 steps done

--- TermProject/utils/visualize.py
+++ TermProject/utils/visualize.py
@@ -21,11 +21,12 @@ def drawLines(img1, img2, lines, pts1, pts2, colors):
     pts2 = np.int32(pts2)
     img1 = cv2.cvtColor(img1, cv2.COLOR_GRAY2BGR)
     img2 = cv2.cvtColor(img2, cv2.COLOR_GRAY2BGR)
-    for r, pt1, pt2, color in zip(lines, pts1, pts2, colors):
+    for i, (r, pt1, pt2, color) in enumerate(zip(lines, pts1, pts2, colors), 1):
         x0,y0 = map(int, [0, -r[2]/r[1] ])
         x1,y1 = map(int, [c, -(r[2]+r[0]*c)/r[1] ])
         img1 = cv2.line(img1, (x0,y0), (x1,y1), color, 1)
         img1 = cv2.circle(img1, tuple(pt1), 5, color, -1)
+        img1 = cv2.putText(img1, '{}'.format(i), tuple(pt1), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 1, cv2.LINE_AA)
         img2 = cv2.circle(img2, tuple(pt2), 5, color, -1)
     return img1, img2
 
@@ -53,9 +54,11 @@ def scatter3DPoints(pts, colors, output_path = None):
     ax.set_xlabel('x axis')
     ax.set_ylabel('y axis')
     ax.set_zlabel('z axis')
-    for pt, c in zip(pts, colors):
-        ax.scatter(pt[0], pt[1], pt[2], c = np.asarray([c]) / 255, marker = 'o')
 
+    for i, (pt, c) in enumerate(zip(pts, colors), 1):
+        ax.scatter(pt[0], pt[1], pt[2], c = np.asarray([c]) / 255, marker = 'o')
+        ax.text(pt[0], pt[1], pt[2], '{}'.format(i), size = 10, zorder = 1, color = np.asarray(c) / 255)
+        
     if output_path is not None:
         if not os.path.exists(output_path):
             os.makedirs(output_path)
